9d4d59c077ae0bd5fe3be5931a5dd35f282f7ba4
Vishwam Subramanyam <v@4261.in>
2013-06-08 21:10:21 -0700

    Adds length limitations for save-bookmark popup
    Adds length checks for the title and description fields in accordance
    with Pinboard limits.

--- save_bookmark.js
+++ save_bookmark.js
@@ -1,7 +1,7 @@
 window.open(
 	'https://pinboard.in/add' 
 	+ '?url='+encodeURIComponent(location.href)	
-	+ '&title='+encodeURIComponent(document.title)
-	+ '&description='+encodeURIComponent(window.getSelection().toString()),
+	+ '&title='+encodeURIComponent(document.title).substr(0, 256)
+	+ '&description='+encodeURIComponent(window.getSelection().toString()).substr(0, 65536),
 	'Pinboard',
 	'toolbar=no,width=700,height=350');
